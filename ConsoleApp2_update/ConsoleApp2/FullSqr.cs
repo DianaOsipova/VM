7ad776b8c43fd57ba4e3ac548676b5cff17e9d41
Diana Osipova <63804049+DianaOsipova@users.noreply.github.com>
2022-03-09 14:33:28 +0300

    Add files via upload

---
+++ ConsoleApp2_update/ConsoleApp2/FullSqr.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace ConsoleApp2
+{
+    class FullSqr
+    {
+
+        public static double[] Calc (double[][] matrix, double[] b)
+        {
+            double[][] a = new double[matrix.Length][];
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                { 
+                    a[j] = new double[matrix[i].Length];
+                    a[j][i] = 0;
+                }
+            }
+            a = MultMatrix(matrix, MatrixTransport(matrix));
+
+            //Console.WriteLine($"Ответ: x{4}: ");
+            return BackTurn(RightTurn(a), b);
+        }
+        public static double[] MultMatrixByVector(double[][] matrix, double[] arr)
+        {
+            double[] result = new double[arr.Length];
+
+            if (matrix.Length != arr.Length)
+                throw new Exception("невозможно умножить матрицу на вектор");
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+               double sum = 0.0;
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    sum += matrix[i][j] * arr[j];
+                }
+                result[i] = sum;
+            }
+            return result;
+        }
+
+        public static void PrintArrayAtConsole(double[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("   " + Math.Round( arr[i], 5));
+            }
+            Console.WriteLine();
+        }
+        public static double[][] MatrixTransport(double[][] matrix)
+        {
+          double[][] result = new double[matrix.Length][];
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    if (i == 0)
+                        result[j] = new double[matrix[i].Length];
+                    result[j][i] = matrix[i][j];
+                }
+            }
+            return result;
+        }
+
+        public static Complex[][] RightTurn(double[][] matrix)
+        {
+            Complex[][] t = new Complex[matrix.Length][];
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    
+                        t[j] = new Complex[matrix[i].Length];
+                    t[j][i] = 0.0;
+                }
+            }
+
+            t[0][0] = Complex.Sqrt(matrix[0][0]);
+            for (int j =1; j < matrix.Length; j++)
+            {
+                t[0][j] = matrix[0][j]/ t[0][0];
+            }
+            Complex sum1 = 0.0;
+            Complex sum2 = 0.0;
+            for (int i = 1; i < matrix.Length; i++)
+            {
+                for (int k = 0; k < i-1; k++)
+                {
+                    sum1 += t[k][i] * t[k][i];
+                }
+                t[i][i] = Complex.Sqrt(matrix[i][i] - sum1);
+                sum1 = 0.0;
+
+                for (int j = i + 1; j < matrix.Length; j++)
+                {
+                    if (i != j)
+                    {
+                        for (int k = 0; k < i - 1; k++)
+                        {
+                            sum2+= t[k][i] * t[k][j];
+                        }
+                        t[i][j] = (matrix[i][j] - sum2) / t[i][i];
+                        sum2 = 0;
+                    }
+                }
+            }
+            return t;
+        }
+
+        public static double[] BackTurn(Complex[][] t, double[] b)
+        {
+            Complex[] y = new Complex[t.Length];
+            double[] x = new double[t.Length];
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                y[i] = 0.0;
+                x[i] = 0.0;
+            }
+
+            Complex sum1 = 0;
+            // sum1 = 0.0;
+            Complex sum2 = 0;
+            //sum2 = 0.0;
+            y[0] = b[0] / t[0][0];
+            for (int i = 1; i < t.Length; i++)
+            {
+                for(int k = 0; k < i-1; k++)
+                {
+                    sum1 += t[k][i] * y[k];
+                }
+                y[i] = (b[i] - sum1) / t[i][i];
+                sum1 = 0.0;
+            }
+
+            x[t.Length-1] = (y[t.Length -1] / t[t.Length - 1][t.Length - 1]).Real;
+            for (int i = t.Length - 2; i >= 0; i--)
+            {
+                for (int k = i+1; k < t.Length; k++)
+                {
+                    sum2 += t[i][k] * x[k];
+                    
+                }
+                x[i] = ((y[i] - sum2) / t[i][i]).Real;
+                sum2 = 0.0;
+            }
+
+            return x;
+        }
+
+
+
+        public static double[][] MultMatrix(double[][] matrix1, double[][] matrix2)
+        {
+            double[][] resultMatrix = new double[matrix1.Length][];
+            
+            for (int i = 0; i < matrix1.Length; i++)
+            {
+                resultMatrix[i] = new double[matrix1.Length];
+                for (int j =0; j< matrix1.Length; j++)
+                resultMatrix[i][j] = 0.0;
+            }
+
+            //throw new Exception();
+
+
+
+            //var matrixC = new int[matrixA.RowsCount(), matrixB.ColumnsCount()];
+
+            for (var i = 0; i < matrix1.Length; i++)
+            {
+                for (var j = 0; j < matrix1[i].Length; j++)
+                {
+                    resultMatrix[i][j] = 0;
+
+                    for (var k = 0; k < matrix1[j].Length; k++)
+                    {
+                        resultMatrix[i][j] += matrix1[i][k] * matrix2[k][j];
+                    }
+                }
+
+            }
+            return resultMatrix;
+        }
+    }
+}
---
+++ ConsoleApp2_update/ConsoleApp2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Numerics;
+
+namespace ConsoleApp2
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            const int n = 5;
+            const int m = 5;
+            const int k = 21; //ОсиповаДианаАндреевна
+
+            double[][] arr = new double[n][];
+            arr[0] = new double[] { 12 + k, 2, m / 4.0, 1, 2 };
+            arr[1] = new double[] { 4, 113 + k, 1, (m / 10.0), m - 4 };
+            arr[2] = new double[] { 1, 2, -24 - k, 3, 4 };
+            arr[3] = new double[] { 1, 2.0 / m, 4, 33 + k, 4 };
+            arr[4] = new double[] { -1, 2, -3, 3 + m, -44 - k };
+
+            double[] b = new double[n] { 1, 2, 3, 4, 5 };
+
+            double eps = Math.Pow(10, -4);
+
+            Console.WriteLine("Начальная матрица:");
+            ConsoleApp2.Yakobi.PrintMatrixAtConsole(arr);
+            Console.WriteLine();
+
+            Console.WriteLine("Методом Якоби:");
+            double[] x = ConsoleApp2.Yakobi.Calc(arr, b, eps);
+            Console.WriteLine("Ответ : ");
+            ConsoleApp2.Yakobi.PrintArrayAtConsole(x);
+            Console.WriteLine("Проверка:");
+            Console.WriteLine("A * X = ");
+            ConsoleApp2.Yakobi.PrintArrayAtConsole(ConsoleApp2.Yakobi.MultMatrixByVector(arr, x));
+            Console.WriteLine();
+
+            Console.WriteLine("Методом квадратного корня:");
+           /* Complex[][] arr2 = new Complex[n][];
+            arr2[0] = new Complex[] { 12 + k, 2, m / 4.0, 1, 2 };
+            arr2[1] = new Complex[] { 4, 113 + k, 1, (m / 10.0), m - 4 };
+            arr2[2] = new Complex[] { 1, 2, -24 - k, 3, 4 };
+            arr2[3] = new Complex[] { 1, 2.0 / m, 4, 33 + k, 4 };
+            arr2[4] = new Complex[] { -1, 2, -3, 3 + m, -44 - k };*/
+
+            //Complex[] b2 = new Complex[n] { 1, 2, 3, 4, 5 };
+            b = ConsoleApp2.FullSqr.MultMatrixByVector(ConsoleApp2.FullSqr.MatrixTransport(arr), b);
+            double[] x1 = ConsoleApp2.FullSqr.Calc(arr, b);
+            Console.WriteLine("Ответ : ");
+            ConsoleApp2.FullSqr.PrintArrayAtConsole(x1);
+            Console.WriteLine("Проверка:");
+            Console.WriteLine("A * X = ");
+            ConsoleApp2.FullSqr.PrintArrayAtConsole(ConsoleApp2.FullSqr.MultMatrixByVector(arr, x1));
+
+            // Console.WriteLine(Complex.Sqrt(-5));
+
+
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
